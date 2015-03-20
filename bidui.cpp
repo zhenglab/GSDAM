9048a4149f6b04119077074f132811cbe105c905
daijialun <zynzl120@163.com>
2015-03-20 10:05:42 +0800

    modify some comments

--- bidui.cpp
+++ bidui.cpp
@@ -5,28 +5,19 @@ using namespace std;
 typedef IplImage* IPL;
 IPL bidui(IPL src1,IPL src2)
 {
-  //const char * inputfile1 = inputfile12.c_str();
-  //const char * inputfile2 = inputfile22.c_str();
-  //const char * outfile = outfile2.c_str();
-  //IplImage * img=0;
-  //IplImage * img2=0;
     int height,width,step,channels;
   int step2,channels2;//height2,width2,s
    int step3,channels3;//height3,width3,
   int i,j,k;
   CvScalar s1,s2;
-      s1.val[0] = 0.000000;
-	s2.val[0] = 255.000000;
+   s1.val[0] = 0.000000;
+  s2.val[0] = 255.000000;
   uchar *data;
   uchar *data2;
   uchar *data3;
-  //cvNamedWindow("src",0);
-  //cvNamedWindow("conter",0);
-  //cvNamedWindow("dst",0);
+
  IPL img=cvCloneImage(src1);
  IPL img2=cvCloneImage(src2);
- //IPL img3=cvCloneImage(src1);
- //cvSetZero(img3);
 
  IplImage * dstImg=cvCreateImage(cvGetSize(img),8,3);
  cvSetZero(dstImg);
@@ -53,15 +44,7 @@ IPL bidui(IPL src1,IPL src2)
 		  }
 		  }
      	  }
-	  //cvShowImage("src",img);
 
- //cvShowImage("conter",img2);
-//cvShowImage("dst",dstImg);
-//cvSaveImage(outfile,dstImg);
- //cvWaitKey(0);
-  //cvDestroyWindow("src");
-  //cvDestroyWindow("conter");
-  //cvDestroyWindow("dst");
   cvReleaseImage(&img);
  cvReleaseImage(&img2);
  return dstImg;
